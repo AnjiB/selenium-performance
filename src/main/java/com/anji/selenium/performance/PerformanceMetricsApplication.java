b67a67ae9202f4f4fbc59637a7f58743dafc797c
anjiboddupally <boddupally.anji@gmail.com>
2024-10-25 20:14:39 +0100

    Created project structure

---
+++ src/main/java/com/anji/selenium/performance/PerformanceMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.anji.selenium.performance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceMetricsApplication.class, args);
+	}
+
+}
